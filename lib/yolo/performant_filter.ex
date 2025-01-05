d70ddc2e254f4aa51ed47c226b6656d72cb924f2
Andrew Pett <astuart.pett@gmail.com>
2025-01-06 12:32:37 +1300

    handle empty tensor possibility

--- lib/yolo/performant_filter.ex
+++ lib/yolo/performant_filter.ex
@@ -3,11 +3,20 @@ defmodule Yolo.PerformantFilter do
 
   # When your NIF is loaded, it will override this function.
   def idx_filter_greater(nx, threshold) do
-    nx
-    |> Nx.to_binary()
-    |> filter_greater_binary(threshold)
-    |> Nx.from_binary(:u64)
+    result =
+      nx
+      |> Nx.to_binary()
+      |> filter_greater_binary(threshold)
+
+    if empty?(result) do
+      []
+    else
+      Nx.from_binary(result, :u64)
+    end
   end
+
+  defp empty?(""), do: true
+  defp empty?(_), do: false
 
   def filter_greater_binary(_binary, _threshold),
       do: :erlang.nif_error(:nif_not_loaded)
